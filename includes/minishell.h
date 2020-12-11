e6c950922bec34c2160fd56c72ad1ab84c8f2150
jjs576 <jjs576@naver.com>
2020-12-11 22:03:18 +0900

    fix tokenize

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: jjoo <jjoo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/20 12:58:02 by jjoo              #+#    #+#             */
-/*   Updated: 2020/12/05 21:06:16 by jjoo             ###   ########.fr       */
+/*   Updated: 2020/12/11 21:34:34 by jjoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,23 @@
 # define NL_BACKSLASH		(1 << 0)
 # define NL_EOF				(1 << 1)
 
+# define TK_DQOUTE			(1 << 0)
+# define TK_QOUTE			(1 << 1)
+# define TK_PIPE			(1 << 2)
+# define TK_SCOLON			(1 << 3)
+
 typedef struct	s_token
 {
-	char			c;
+	char			*str;
+	int				flag;
 	struct s_token	*next;
 }				t_token;
+
+t_token			*token_new(char *str);
+t_token			*token_last(t_token **head);
+void			token_update(t_token **head, char *key, char *value);
+void			token_delete(t_token **head, char *str);
+void			token_push_back(t_token **head, char *str);
 
 typedef struct	s_env
 {
@@ -53,6 +65,7 @@ void			env_push_back(t_env **head, char *key, char *value);
 typedef struct	s_info
 {
 	t_env	*env;
+	t_token	*token;
 	char	pwd[MAX_PATH_LENGTH];
 	char	oldpwd[MAX_PATH_LENGTH];
 	char	input[MAX_STR];
--- srcs/tokenize.c
+++ srcs/tokenize.c
@@ -6,10 +6,25 @@
 /*   By: jjoo <jjoo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/05 21:05:31 by jjoo              #+#    #+#             */
-/*   Updated: 2020/12/05 21:05:54 by jjoo             ###   ########.fr       */
+/*   Updated: 2020/12/11 22:01:06 by jjoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	tokenize(t_info *info);
+void	tokenize(t_info *info)
+{
+	int		i;
+	int		cur_quote;
+
+	i = -1;
+	cur_quote = 0;
+	info->token = token_new("");
+	while (info->input[++i])
+	{
+		if (info->input)
+		{
+
+		}
+	}
+}
