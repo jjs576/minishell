e077f393748971cbe4de2f416687ea9721e809e6
yepark <poisonF2@naver.com>
2021-01-02 20:33:16 +0900

    edit ft_echo.c

--- srcs/ft_echo.c
+++ srcs/ft_echo.c
@@ -11,33 +11,33 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+
 /*
-void ft_echo(t_info *s, t_cmd cmd)
+void ft_echo(t_info *info)
 {
 	int	i;
 	int	flag;
 
 	flag = 1;
 	i = 1;
-	if (cmd.argv_num > 1)
+	if (info->cmd.argc > 2)
 	{
-		while (cmd.argv[i] && (ft_strcmp(cmd.argv[i], "-n") == 0))
+		while (info->cmd.argv[i] && (ft_strcmp(info->cmd.argv[i], "-n") == 0))
 		{
 			flag = 0;
 			i++;
 		}
 	}
-	while (cmd.argv[i])
+	while (info->cmd.argv[i])
 	{
-		ft_printf("%s", cmd.argv[i]);
-		if (cmd.argb[i + 1] != NULL)
+		ft_printf("%s", info->cmd.argv[i]);
+		if (info->cmd.argv[i + 1] != NULL)
 			ft_printf(" ");
 		i++;
 	}
 	if (flag)
 		ft_printf("\n");
-	s->ret = 0;
+	info->returned = 0;
 	exit(0);
 }
-
 */
