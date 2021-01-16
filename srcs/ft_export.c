ab0a8151750c80af5096d16bfbce16383c8e92b2
yepark <poisonF2@naver.com>
2021-01-16 14:54:58 +0900

    Add export base

--- srcs/ft_export.c
+++ srcs/ft_export.c
@@ -12,9 +12,38 @@
 
 #include "minishell.h"
 
-void	ft_export(t_info *info, t_command *cmd)
+static int	print_export(t_env *env)
 {
+	while (env)
+	{
+		if (env->value == NULL)
+			ft_printf("declare -x %s \n", env->key);
+		else
+			ft_printf("declare -x %s=\"%s\"\n", env->key, env->value);
+		env = env->next;
+	}
 
-	(void)info;
-	(void)cmd;
+	return //뭐로 해놧니?
+}
+
+void		ft_export(t_info *info, t_command *cmd)
+{
+	char	**str;
+	int		i;
+
+	i = 0;
+	ft_bzero(buf, MAX_PATH_LENGTH);
+	if (cmd->argc == 1)
+		return (print_export(info->env));
+	else
+	{
+		//valid key값 해줘야된
+		while (++i < cmd->argc)
+		{
+			str = ft_split(argv[1], '=');
+			env_update(&info->env, str[0], str[1]);
+			free_2d(str); 
+		}
+	}
+	//return 이랑 직전의 리턴값 넣는애에 넣어주기
 }
