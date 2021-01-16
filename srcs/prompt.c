c66b4b0c97aa1958cbb1069625cd8d58f6ac8833
jjs576 <jjs576@naver.com>
2021-01-16 22:38:03 +0900

    Fix : prompt

--- srcs/prompt.c
+++ srcs/prompt.c
@@ -6,7 +6,7 @@
 /*   By: jjoo <jjoo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 18:02:09 by jjoo              #+#    #+#             */
-/*   Updated: 2021/01/16 18:26:45 by jjoo             ###   ########.fr       */
+/*   Updated: 2021/01/16 22:37:19 by jjoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,10 @@ static char	check_eof(char *s)
 	flag = NL_EOF;
 	if (ft_strlen(s) == 0)
 	{
-		ft_printf("exit\n");
+		ft_printf("  \b\b\nexit\n");
 		exit(0);
 	}
+	ft_printf("  \b\b");
 	return (flag);
 }
 
